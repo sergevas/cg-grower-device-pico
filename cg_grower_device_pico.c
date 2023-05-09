6539baf51d55e1765b10888e18b341f4028f4381
sergevas <brighterr@gmail.com>
2023-05-09 23:42:48 +0300

    Add pump run button

--- cg_grower_device_pico.c
+++ cg_grower_device_pico.c
@@ -9,6 +9,7 @@
 #define ADC_INPUT_0 0
 // #define ADC_INPUT_4 4
 #define WATER_PUMP_PIN 22
+#define WATER_PUMP_RUN_PIN 3
 #define TEMP_SENSOR_DS18B20_PIN 2
 
 #define MOVING_AVERAGE_PERIOD 32
@@ -195,6 +196,51 @@ void read_temperature(float sensor_seq_readings[])
     }
 }
 
+static char event_str[128];
+
+void gpio_event_string(char *buf, uint32_t events);
+
+void gpio_callback(uint gpio, uint32_t events)
+{
+    // Put the GPIO event(s) that just happened into event_str
+    // so we can print it
+    gpio_event_string(event_str, events);
+    printf("GPIO %d %s\n", gpio, event_str);
+}
+
+static const char *gpio_irq_str[] = {
+    "LEVEL_LOW",  // 0x1
+    "LEVEL_HIGH", // 0x2
+    "EDGE_FALL",  // 0x4
+    "EDGE_RISE"   // 0x8
+};
+
+void gpio_event_string(char *buf, uint32_t events)
+{
+    for (uint i = 0; i < 4; i++)
+    {
+        uint mask = (1 << i);
+        if (events & mask)
+        {
+            // Copy this event string into the user string
+            const char *event_str = gpio_irq_str[i];
+            while (*event_str != '\0')
+            {
+                *buf++ = *event_str++;
+            }
+            events &= ~mask;
+
+            // If more events add ", "
+            if (events)
+            {
+                *buf++ = ',';
+                *buf++ = ' ';
+            }
+        }
+    }
+    *buf++ = '\0';
+}
+
 int main()
 {
     stdio_init_all();
@@ -221,7 +267,13 @@ int main()
 
     float soil_moisture_raw, soil_moisture_percent, temp;
 
-    sleep_ms(10000);
+    gpio_set_function(WATER_PUMP_PIN, GPIO_FUNC_SIO);
+    gpio_set_dir(WATER_PUMP_PIN, true);
+
+    gpio_set_function(WATER_PUMP_RUN_PIN, GPIO_FUNC_SIO);
+    gpio_set_dir(WATER_PUMP_RUN_PIN, false);
+    gpio_pull_down(WATER_PUMP_RUN_PIN);
+    gpio_set_irq_enabled_with_callback(WATER_PUMP_RUN_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
     for (;;)
     {
